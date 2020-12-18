552a52b7431853e13506e9618f8865c8dd458419
al <alxayeed@gmail.com>
2020-12-18 11:39:26 +0600

    code restructered

--- 02-counter/main.js
+++ 02-counter/main.js
@@ -1,30 +1,57 @@
-let value = document.getElementById("value")
-let counterValue = parseInt(value.textContent)
-const increase = document.querySelector(".increase")
-const reset = document.querySelector(".reset")
-const decrease = document.querySelector(".decrease")
+const value = document.getElementById("value")
+const btns = document.querySelectorAll('.btn')
+let count = 0
 
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("increase")) {
+            count++
+            value.textContent = count
+            if (count > 0) {
+                value.style.color = "green"
+            }
+        }
+        else if (styles.contains("decrease")) {
+            count--
+            value.textContent = count
+            if (count < 0) {
+                value.style.color = "red"
+            }
+        }
+        else {
+            count = 0
+            value.textContent = count
+            if (count == 0) {
+                value.style.color = "black"
+            }
+        }
 
+    })
 
-increase.addEventListener('click', function () {
-    counterValue += 1
-    value.textContent = counterValue
-    if (counterValue > 0) {
-        value.style.color = 'blue'
-    }
 })
 
 
-decrease.addEventListener('click', function () {
-    counterValue -= 1
-    value.textContent = counterValue
-    if (counterValue < 0) {
-        value.style.color = 'red'
-    }
-})
 
-reset.addEventListener('click', function () {
-    counterValue = 0
-    value.textContent = counterValue
-    value.style.color = '#000'
-})
+// increase.addEventListener('click', function () {
+//     counterValue += 1
+//     value.textContent = counterValue
+//     if (counterValue > 0) {
+//         value.style.color = 'blue'
+//     }
+// })
+
+
+// decrease.addEventListener('click', function () {
+//     counterValue -= 1
+//     value.textContent = counterValue
+//     if (counterValue < 0) {
+//         value.style.color = 'red'
+//     }
+// })
+
+// reset.addEventListener('click', function () {
+//     counterValue = 0
+//     value.textContent = counterValue
+//     value.style.color = '#000'
+// })
